9a66a10f473f83705fb0ea896a2c45cfbec5a18c
Yasunari19 <108570080+Yasunari19@users.noreply.github.com>
2022-07-09 09:25:13 +0900

    Add files via upload

---
+++ CustomDelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class CustomDelegateCommand : ICommand
+    {
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        public CustomDelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _Execute?.Invoke(parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute?.Invoke(parameter) ?? true;
+        }
+    }
+}
